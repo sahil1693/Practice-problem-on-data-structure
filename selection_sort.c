b456ec7ff1b2c588ce27f4f9ed443535122353fe
sahil1693 <sahi171693.cse@chitkara.edu.in>
2020-01-29 18:32:24 +0530

    selection_sort.c

---
+++ selection_sort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    int a[n];
+
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++){
+        int temp;
+        int small=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[small]>a[j])
+                small=j;
+        }
+        temp=a[small];
+        a[small]=a[i];
+        a[i]=temp;
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }   
+    return 0;
+}
